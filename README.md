0e654aa155a9614c77318fc26942d9b34c4efa7f
Nathan Ringo <remexre@gmail.com>
2018-01-21 18:03:36 -0600

    Updates for log 0.4

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # pretty_logger
 
+[![Build Status](https://travis-ci.org/remexre/pretty_logger.svg?branch=master)](https://travis-ci.org/remexre/pretty_logger)
+[![Crates.io](https://img.shields.io/crates/v/pretty_logger.svg)](https://crates.io/crates/pretty_logger)
+[![Documentation](https://docs.rs/pretty_logger/badge.svg)](https://docs.rs/pretty_logger/*/pretty_logger/)
+![License](https://img.shields.io/crates/l/pretty_logger.svg)
+
 A pretty logger.
 
 A logger similar to `pretty_env_logger`, but configured from the `init` function instead of from an environment variable.
 
 It also supports falling back to a non-colored logger.
+
+## License
+
+Licensed under either of
+
+ * Apache License, Version 2.0, (http://www.apache.org/licenses/LICENSE-2.0)
+ * MIT license (http://opensource.org/licenses/MIT)
+
+at your option.
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion in the work by you, as defined in the Apache-2.0 license, shall be dual licensed as above, without any additional terms or conditions.
--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@ use std::io::{stderr, stdout, Write};
 use std::sync::atomic::{AtomicUsize, Ordering};
 
 use ansi_term::{ANSIGenericString, Colour, Style};
-use log::{Log, LogLevel, LogLevelFilter, LogMetadata, LogRecord, SetLoggerError};
+use log::{Level, LevelFilter, Log, Metadata, Record, SetLoggerError};
 use unicode_segmentation::UnicodeSegmentation;
 
 /// Where to log errors to.
@@ -57,16 +57,16 @@ impl Default for Destination {
 }
 
 /// The logger.
-/// 
+///
 /// The defaults are:
 ///
-///  - Log to `stdepretty_env_logger/0.1.1/prettyrr`
+///  - Log to `stderr`
 ///  - Log at the info level and higher.
 ///  - Use the default theme (see the [`Theme`](struct.Theme.html) type for details).
 ///  - Use color iff `stderr` is a TTY
 pub struct Logger {
     destination: Destination,
-    level: LogLevelFilter,
+    level: LevelFilter,
     max_module_width: AtomicUsize,
     max_target_width: AtomicUsize,
     theme: Theme,
@@ -74,7 +74,11 @@ pub struct Logger {
 
 impl Logger {
     /// Creates a new instance of Logger.
-    pub fn new(destination: Destination, level: LogLevelFilter, theme: Theme) -> Logger {
+    pub fn new(
+        destination: Destination,
+        level: LevelFilter,
+        theme: Theme,
+    ) -> Logger {
         Logger {
             destination,
             level,
@@ -86,17 +90,19 @@ impl Logger {
 
     /// Sets this logger as the global logger.
     pub fn set_logger(self) -> Result<(), SetLoggerError> {
-        log::set_logger(|m| {
-            m.set(self.level);
-            Box::new(self)
-        })
+        log::set_boxed_logger(Box::new(self))
     }
 
     fn update_module_width(&self, width: usize) -> usize {
         loop {
             let old = self.max_module_width.load(Ordering::SeqCst);
             let new = max(old, width);
-            if self.max_module_width.compare_and_swap(old, new, Ordering::SeqCst) == old {
+            if self.max_module_width.compare_and_swap(
+                old,
+                new,
+                Ordering::SeqCst,
+            ) == old
+            {
                 return new;
             }
         }
@@ -106,7 +112,12 @@ impl Logger {
         loop {
             let old = self.max_target_width.load(Ordering::SeqCst);
             let new = max(old, width);
-            if self.max_target_width.compare_and_swap(old, new, Ordering::SeqCst) == old {
+            if self.max_target_width.compare_and_swap(
+                old,
+                new,
+                Ordering::SeqCst,
+            ) == old
+            {
                 return new;
             }
         }
@@ -121,35 +132,52 @@ impl Default for Logger {
         } else {
             Theme::empty()
         };
-        Logger::new(destination, LogLevelFilter::Info, theme)
+        Logger::new(destination, LevelFilter::Info, theme)
     }
 }
 
 impl Log for Logger {
-    fn enabled(&self, metadata: &LogMetadata) -> bool {
-        self.level.to_log_level().map(|level| {
-            metadata.level() <= level
-        }).unwrap_or(false)
+    fn enabled(&self, metadata: &Metadata) -> bool {
+        self.level
+            .to_level()
+            .map(|level| metadata.level() <= level)
+            .unwrap_or(false)
     }
 
-    fn log(&self, record: &LogRecord) {
+    fn flush(&self) {}
+
+    fn log(&self, record: &Record) {
         if !self.enabled(record.metadata()) {
             return;
         }
 
-        let module = record.location().module_path();
+        let module = record.module_path().unwrap_or("<unknown>");
         let target = record.target();
-        let module_length = self.update_module_width(module.graphemes(true).count());
+        let module_length =
+            self.update_module_width(module.graphemes(true).count());
 
         let _ = if module == target {
-            writeln!(self.destination.write(), "{}|{:.*}|{}",
-                self.theme.paint_log_level(record.level()), module_length,
-                module, record.args())
+            writeln!(
+                self.destination.write(),
+                "{}|{:.*}|{}",
+                self.theme.paint_log_level(record.level()),
+                module_length,
+                module,
+                record.args()
+            )
         } else {
-            let target_length = self.update_target_width(target.graphemes(true).count());
-            writeln!(self.destination.write(), "{}|{:.*}|{:.*}|{}",
-                self.theme.paint_log_level(record.level()), module_length,
-                module, target_length, target, record.args())
+            let target_length =
+                self.update_target_width(target.graphemes(true).count());
+            writeln!(
+                self.destination.write(),
+                "{}|{:.*}|{:.*}|{}",
+                self.theme.paint_log_level(record.level()),
+                module_length,
+                module,
+                target_length,
+                target,
+                record.args()
+            )
         };
     }
 }
@@ -199,13 +227,16 @@ impl Theme {
     }
 
     /// Paints a log level with a theme.
-    pub fn paint_log_level(&self, level: LogLevel) -> ANSIGenericString<'static, str> {
+    pub fn paint_log_level(
+        &self,
+        level: Level,
+    ) -> ANSIGenericString<'static, str> {
         let (style, name) = match level {
-            LogLevel::Error => (self.error, "ERROR"),
-            LogLevel::Warn  => (self.warn,  "WARN "),
-            LogLevel::Info  => (self.info,  "INFO "),
-            LogLevel::Debug => (self.debug, "DEBUG"),
-            LogLevel::Trace => (self.trace, "TRACE"),
+            Level::Error => (self.error, "ERROR"),
+            Level::Warn => (self.warn, "WARN "),
+            Level::Info => (self.info, "INFO "),
+            Level::Debug => (self.debug, "DEBUG"),
+            Level::Trace => (self.trace, "TRACE"),
         };
         style.paint(name)
     }
@@ -225,14 +256,18 @@ impl Default for Theme {
 }
 
 /// Initializes the global logger.
-pub fn init(destination: Destination, level: LogLevelFilter, theme: Theme) -> Result<(), SetLoggerError> {
+pub fn init(
+    destination: Destination,
+    level: LevelFilter,
+    theme: Theme,
+) -> Result<(), SetLoggerError> {
     platform_init();
     Logger::new(destination, level, theme).set_logger()
 }
 
 /// Initializes the global logger to log at the given level, using the defaults
 /// for other fields.
-pub fn init_level(level: LogLevelFilter) -> Result<(), SetLoggerError> {
+pub fn init_level(level: LevelFilter) -> Result<(), SetLoggerError> {
     platform_init();
     let mut logger = Logger::default();
     logger.level = level;
